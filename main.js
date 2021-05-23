58a6ee74ac35edfb5f6911712dbc1ddce9d7db8f
namanvikas <79072465+namanvikas@users.noreply.github.com>
2021-05-23 10:04:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function preload(){
+musthaj_x=1;
+musthaj_y=0;
 
+function preload(){
+clown_image=loadImage("https://i.postimg.cc/3JcbYb2h/m.png");
 }
 function setup(){
     Canvas=createCanvas(300,300);
@@ -23,6 +26,8 @@ function modelloaded(){
 function gotposes(results){
     if(results.length>0){
         console.log(results);
+        musthaj_x=results[0].pose.musthaj.x-12;
+        musthaj_y=results[0].pose.musthaj.y-12;
         console.log("nose x="+results[0].pose.nose.x);
         console.log("nose y="+results[0].pose.nose.y);
     }
